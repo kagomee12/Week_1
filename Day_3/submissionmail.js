68365c1181ca5fe9958a5d950ac87a721a63a4b1
kagomee12 <kalfitrah@gmail.com>
2024-05-30 22:50:23 +0700

    Day_3

---
+++ Day_3/submissionmail.js
@@ -0,0 +1,21 @@
+
+function submitContact() {
+    let nama = document.getElementById("nama").value;
+    let email = document.getElementById("email").value;
+    let number = document.getElementById("phonenumber").value;
+    let profesi = document.getElementById("profesi").value;
+    let isipesan = document.getElementById("isipesan").value;
+    let a =document.createElement('a');
+    const myEmail = "kiagus.119160055@student.itera.ac.id";
+
+    a.href = `mailto:${myEmail}?subject= ${profesi}&body= halo saya ${nama}, ${isipesan}`
+
+    if (nama == "" || email == "" || number == "" || profesi == "value1" || isipesan == "") {
+        alert("semua kolom harus diisi");
+    }else {
+        alert("pesan anda sudah terkirim");
+        btnemail.type = "submit";
+        a.click()
+    }
+
+  }
